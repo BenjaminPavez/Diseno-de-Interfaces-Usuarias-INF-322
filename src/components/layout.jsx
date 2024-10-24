eb7bffabb681d14694adf528d1e7ac70594d1408
Nicolas <nicolas.olivosm@usm.cl>
2024-10-24 19:36:34 -0300

    Login part1
    Prueba de Login (No va a estar en la navBar)

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home_page'
 import LightbulbPage from '../pages/lightbulb_page'
+import Login from '../pages/login'
 
 import NavBar from '../components/nav_bar'
 import Header from '../components/header'
@@ -18,6 +19,7 @@ const Layout = () => {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/lightbulb' element={<LightbulbPage />} />
+            <Route path='/login' element={<Login />} />
           </Routes>
         </div>
         
